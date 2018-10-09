8eec167bea78ab897b034b8f723813bf2d970793
malcolm stanley <a.malcolm.stanley@gmail.com>
2018-10-08 22:59:07 -0400

    powerpoint in place

--- PrecinctSignage.py
+++ PrecinctSignage.py
@@ -1,2 +1,15 @@
-#this is a comment
+from pptx import Presentation
+from pptx.util import Inches
+import csv
 
+prs = Presentation("C:\Users\amalc\OneDrive\Resistor\Code\PrecinctSignage\PrecinctSignageTemplate.pptx")
+title_slide_layout = prs.slide_layouts[1]
+blank_slide_layout = prs.slide_layouts[2]
+
+
+
+
+with open('names.csv') as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        print(row['first_name'], row['last_name'])
